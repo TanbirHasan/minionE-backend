5db19d9bc1e9e7110bc94c9869818c3b13909146
sajid9962 <clashking1545@gmail.com>
2023-03-01 11:39:58 +0600

    Contact message api created

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ProductRoutes = require('./routes/products');
 const UserRoutes = require('./routes/users');
 const CategoriesRoutes = require('./routes/categories');
 const ReviewsRoutes = require('./routes/reviews');
+const ContactRouter = require('./routes/contactUs');
 
 // middleware
 app.use(express.json());
@@ -18,6 +19,7 @@ app.use('/products', ProductRoutes);
 app.use('/users', UserRoutes);
 app.use('/categories', CategoriesRoutes);
 app.use('/reviews', ReviewsRoutes);
+app.use('/contact-messages', ContactRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello I am live');
